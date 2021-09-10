0290e08d92c254939ba6e42a50f85b9e80e5e6ea
AnkitaGharde <84709511+AnkitaGharde@users.noreply.github.com>
2021-09-10 15:35:09 +0530

    Add files via upload

---
+++ Proftfolio_Project/resumedj/core/forms.py
@@ -0,0 +1,13 @@
+# from django import forms
+# from .models import Resumetable
+# class resumeform(forms.ModelForm):
+#     class Meta:
+#         model=Resumetable
+#         fields=['YourName','YourEmail','YourSubject','YourMessage']
+
+#         # widgets={
+#         #     'YourName':forms.TextInput(attrs={'class':'form-control' }),
+#         #     ' YourEmail ': forms.EmailInput(attrs={'class': 'form-control'}),
+#         #     'YourSubject': forms.TextInput(attrs={'class':'form-control' }),
+#         #     'YourMessage':forms.TextInput(attrs={'class':'form-control' }),
+#         # }
